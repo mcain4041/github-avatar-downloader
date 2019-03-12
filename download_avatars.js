19eea3a4bfaeb3c4669d5c4e9dd12ef9fb2b14ad
Michael Cain <michaelcaincb@gmail.com>
2019-03-12 03:09:49 +0000

    call back token private file

--- download_avatars.js
+++ download_avatars.js
@@ -1,6 +1,5 @@
-
 var request = require("request");
-var pw = require('./secrets')
+var pw = require("./secrets");
 
 console.log("Welcome to the GitHub Avatar Downloader!");
 
@@ -13,20 +12,23 @@ function getRepoContributors(repoOwner, repoName, cb) {
       repoName +
       "/contributors",
     headers: {
-      "User-Agent": "Cain310"
-      "Authorization": ""
+      "User-Agent": "Cain310",
+      Authorization: pw.GITHUB_TOKEN
     }
   };
   request(options, function(err, res, body) {
-    cb(err, body);
+    var data = JSON.parse(body);
+    cb(err, data);
   });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
+  for (var i = 0; i < result.length; i++) {
+    console.log("Result:", result[i].avatar_url);
+  }
+  // console.log("Errors:", err);
+  // console.log("Result:", result);
 });
-
 
 // 3d99ad55588c550ecd66cf01522d54f0b775
 
